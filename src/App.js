d6496be0c607ced92e0b0c618ff47e083de31641
LE PIERRES Erwan <erwan-le-pierre@isoskele.fr>
2023-06-29 11:57:09 +0200

    fix(homePage): improve style homePage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React, { useState } from 'react';
-import { ChakraProvider, Box, Heading, Button } from '@chakra-ui/react';
+import { 
+  ChakraProvider, 
+  Box, 
+  Heading, 
+  Button,
+  Container,
+  Text,  
+  Stack,
+  Icon,
+  useColorModeValue,
+  createIcon, 
+} from '@chakra-ui/react';
 import Quiz from './components/Quiz';
 import ScoreInput from './components/ScoreInput';
 import ScoreMenu from './components/ScoreMenu';
@@ -21,17 +32,49 @@ const App = () => {
   return (
     <ChakraProvider>
       <Box p={4}>
-        <Heading as="h1" size="xl" mb={4}>
-          Random Quiz App
-        </Heading>
-        <Box mb={4}>
-          <Button onClick={handleQuizStart} colorScheme="blue">
-            Start Quiz
-          </Button>
-        </Box>
-        <Quiz questions={questions} />
-        <ScoreInput onSubmit={handleScoreSubmit} />
-        <ScoreMenu scores={scores} />
+        <Container maxW={'3xl'}>
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}>
+              Burger de la Mort <br />
+            <Text as={'span'} color={'green.400'}>
+              MAZ 2023
+            </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+            Bienvenue dans la folie du "Burger de la Mort" ! C'est parti pour un tourbillon de 10 questions où ton cerveau va s'entortiller ! Mais attention, pas de triche, tu dois retenir les réponses jusqu'à la fin. C'est comme manger un burger géant sans perdre le fromage entre les tranches. Prêt à te régaler d'un défi cérébral savoureux ?
+          </Text>
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}>
+            <Button
+              onClick={handleQuizStart}
+              colorScheme={'green'}
+              bg={'green.400'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'green.500',
+              }}>
+              Let's go !!
+            </Button>
+            <Button 
+              rounded={'full'} colorScheme={'blue'} size={'sm'} 
+              px={6}>
+              Classement
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
       </Box>
     </ChakraProvider>
   );
